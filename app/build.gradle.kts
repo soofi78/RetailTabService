66a6c4da5a10d0b0114cb94b4b418519262dd626
Faheem Ahmad <faheem.a1992@gmail.com>
2024-08-19 02:12:53 +0500

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.lfsolutions.retail"
         minSdk = 24
         targetSdk = 34
-        versionCode = 33
-        versionName = "0.0.33"
+        versionCode = 35
+        versionName = "0.0.35"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
